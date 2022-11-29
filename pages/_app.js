3d0d450876f3015dc9e4682b809c5d5e754b46e0
Jacob-alwin <alwinjacobjoy@gmail.com>
2022-11-30 00:07:48 +0530

    #6

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
+
+  const [theme, setTheme] = useState("light");
   return <Component {...pageProps} />
 }
 

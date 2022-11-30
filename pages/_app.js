1b6814fe3286ad71a522551c7defdf920ea61da0
Jacob-alwin <alwinjacobjoy@gmail.com>
2022-11-30 16:58:04 +0530

    #13

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import { useState, useEffect } from "react";
 import "../styles/globals.scss";
 import AOS from "aos";
+
 import "aos/dist/aos.css";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     AOS.init();
-  });
+  },[]);
 
   const [theme, setTheme] = useState("light");
   return <Component {...pageProps} />;
